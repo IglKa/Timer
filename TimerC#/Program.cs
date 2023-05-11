63d16b4c007997012a44a5db210c4f8269a33019
Игорь <superigorek74@gmail.com>
2023-05-12 01:09:15 +0500

    Add project files.

---
+++ TimerC#/Program.cs
@@ -0,0 +1,20 @@
+﻿using TimerC_;
+
+
+namespace ConsolTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many minutes to study? (integers only)");
+            string MinsToStudy = Console.ReadLine();
+            Console.WriteLine("What is your rest time? (integers only)");
+            string MinsToRest = Console.ReadLine();
+
+            var timer = new TimerC_.Timer(MinsToStudy, MinsToRest);
+
+            timer.Start();
+        }
+    }
+}
